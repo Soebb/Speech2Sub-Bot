29585f26a355ee68cfd7c7d8dcab8aabb8337b6e
Soebb <87156166+Soebb@users.noreply.github.com>
2022-02-16 21:13:58 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -113,8 +113,7 @@ def ds_process_audio(audio_file, file_handle):
     global line_count
     # File name contains start and end times in seconds. Extract that
     limits = audio_file.split("/")[-1][:-4].split("_")[-1].split("-")
-
-    target_piece = input_as_wave[round(int(limits[0])) * 1000:round(int(limits[1])) * 1000]
+    target_piece = input_as_wave[int(limits[0].split('.')[0]) * 1000 + int(limits[0].split('.')[1][:1]+"00"):int(limits[1].split('.')[0]) * 1000 + int(limits[1].split('.')[1][:1]+"00")]
     wf = wave.open(target_piece, "rb")
     data = wf.readframes(wf.getnframes())
     if rec.AcceptWaveform(data):
